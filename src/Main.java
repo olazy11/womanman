f1a853102dbf788832c12754ec4cd21a29103edb
EvgenAleks <evgenii.aleksandroff@gmail.com>
2023-04-13 20:46:15 +0200

    new woman

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        
+
+Woman eve = new Woman("22", "Fine");
+        eve.womanInfo();
 
         System.out.println("Hello from woman!");
     }
